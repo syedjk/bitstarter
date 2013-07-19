d9f52c46fb635c5388f37e8f35eecb97f102e147
Junaid Syed <syedjk@live.ca>
2013-07-19 06:27:05 +0000

    edit web.js

--- web.js
+++ web.js
@@ -2,11 +2,13 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var indexContent = fs.readFileSync("index.html");
+
 var buffer = new Buffer(16);
 buffer.write("hello","utf-8");
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
